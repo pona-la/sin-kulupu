21a8ce880873da0d693fae9c4d19aa3f0222fcdc
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2025-04-09 01:06:40 +0500

    improve formatting

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "📰 sin kulupu";
+export const SITE_TITLE = "sin kulupu";
 export const SITE_DESCRIPTION = "A Toki Pona newsletter.";
