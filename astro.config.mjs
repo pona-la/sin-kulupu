0009e71703462ab713aaaa577fc32ed2325cca9d
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2025-04-09 03:47:42 +0500

    provide base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://acipensersturio.github.io/sin-kulupu",
+  site: "https://acipensersturio.github.io",
+  base: "sin-kulupu",
   integrations: [
     // mdx(),
     sitemap(),
