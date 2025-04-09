6d5771f878438f584d4b7bf2799876ce7ca48cd7
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2025-04-09 19:23:27 +0500

    meta tag improvements

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "sin kulupu";
-export const SITE_DESCRIPTION = "A Toki Pona newsletter.";
+export const SITE_DESCRIPTION =
+  "A Toki Pona newsletter. Keep in touch with Toki Pona events and creative works. Every two weeks.";
