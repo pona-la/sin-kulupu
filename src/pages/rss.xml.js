f69736386c4bd3cbb2efd1c4c86ebb47db5d9c9e
Slava <53832230+slavalamp@users.noreply.github.com>
2025-04-22 23:07:18 +0200

    Add publication dates to the RSS feed (#4)
    * add post publication dates to rss
    
    * add post publication dates to rss correctly

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -14,6 +14,7 @@ export async function GET(context) {
       ...post.data,
       link: `${import.meta.env.BASE_URL}/${post.id}/`,
       content: post.rendered.html,
+      pubDate: post.data.date,
     })),
   });
 }
