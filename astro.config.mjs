cca1c94440f0db944ddee622011db39f607baba6
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2025-10-10 16:07:31 +0500

    refactor: remove a hardcoded url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,14 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import { SITE_URL } from "./src/consts";
 
 const deploy = import.meta.env.PROD
-  ? { site: "https://sin-kulupu.pona.la/", base: "./" }
-  : { site: "http://localhost/", base: "./" };
+  ? { site: `https://${SITE_URL}/` }
+  : { site: "http://localhost/" };
 
 // https://astro.build/config
 export default defineConfig({
   ...deploy,
-  ...{
-    integrations: [mdx(), sitemap()],
-  },
+  integrations: [mdx(), sitemap()],
 });
